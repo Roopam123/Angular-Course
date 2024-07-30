93b09f59adc7090509f7b06135dace9c3360f736
Roopam <roopamr2411@gmail.com>
2024-07-30 13:44:18 +0530

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { PropertyBindingComponent } from './components/property-binding/property-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,PropertyBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  name: string = "Khumesh";
+  status: boolean = false;
   title = 'anguler-app';
+  getMyName(){
+    return "My Name is Nidhu"
+  }
 }
---
+++ src/app/components/property-binding/property-binding.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-binding',
+  standalone: true,
+  imports: [],
+  templateUrl: './property-binding.component.html',
+  styleUrl: './property-binding.component.css'
+})
+export class PropertyBindingComponent {
+  constructor(){}
+  collageName:string="BUIT BHOPAl"
+  enbaled:boolean=true
+}
